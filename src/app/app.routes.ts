2f70b889c5b901e6401f234ecad0a2accf6180d0
Luc Guinchard <luc@guinchard.org>
2024-01-13 15:43:38 +0100

    add rooting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { BlogComponent } from './page/blog/blog.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+    title: 'Blog page',
+  },
+];
